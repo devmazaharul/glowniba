059473060d8293c7b0ae7a9c2e27c4c907b4a037
Snk. Eazul <mazaharul>
2025-04-27 15:18:55 +0600

    bug find

--- constants/index.ts
+++ constants/index.ts
@@ -1,32 +1,30 @@
-
-export const  defualtValue=Object.freeze({
-  saltRound:10,
-  addProductLimit:50,
-  featureListSize:10,
-  productLimit:10,
-  productLimitMobile:5,
-  productLimitTablet:8,
-  productLimitDesktop:10,
-  siteName:"Glow niba",
-  siteUrl:"https://glowniba.vercel.app",
-  facbookPageUrl:"https://facebook.com/glowniba",
-  instagramUrl:"http://instagram.com/glowniba",
-  instagramUserName:"@glowniba",
-  whatsAppNumber:"8801799588",
-  whatAppsMessage:"Hi, I want to buy from Glow Niba!",
-})
+export const defualtValue = Object.freeze({
+  saltRound: 10,
+  addProductLimit: 50,
+  featureListSize: 10,
+  productLimit: 10,
+  productLimitMobile: 5,
+  productLimitTablet: 8,
+  productLimitDesktop: 10,
+  siteName: 'Glow niba',
+  siteUrl: 'https://glowniba.vercel.app',
+  facbookPageUrl: 'https://facebook.com/glowniba',
+  instagramUrl: 'http://instagram.com/glowniba',
+  instagramUserName: '@glowniba',
+  whatsAppNumber: '8801799588',
+  whatAppsMessage: 'Hi, I want to buy from Glow Niba!',
+});
 
 export const contactInfo = Object.freeze({
-  supprtyEmail: "support@glowniba.com",
-  supprtyPhone: "+880171265885",
-  supprtyAddress: "Dhaka, Bangladesh",
-  supprtyLocation: "Dhaka, Bangladesh",
-})
+  supprtyEmail: 'support@glowniba.com',
+  supprtyPhone: '+880171265885',
+  supprtyAddress: 'Dhaka, Bangladesh',
+  supprtyLocation: 'Dhaka, Bangladesh',
+});
 
-
-export const  loggerControllerExternalObject=Object.freeze({
-  useExternalLogserver:true,
-  loggerServerHost:"localhost",
-  loggerServerPort:8080,
-  loggerServerPath:"/log",
-})
+export const loggerControllerExternalObject = Object.freeze({
+  useExternalLogserver: false,
+  loggerServerHost: 'localhost',
+  loggerServerPort: 8080,
+  loggerServerPath: '/log',
+});
