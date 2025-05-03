08f3f6203be27e6cb567d4bb3ead77f791909e6e
mazaharul islam <mazaharul>
2025-05-03 19:19:40 +0600

    added

--- action/product.ts
+++ action/product.ts
@@ -122,11 +122,11 @@ export const getProducts = async (limit: number = 10, page: number = 1) => {
 
 export const getProductbySlug = async (productslug: string) => {
   try {
-    const slug = decodeURIComponent(productslug)
-    const findProduct = await Product.findOne({ slug });
+   // const slug = decodeURIComponent(productslug)
+    const findProduct = await Product.findById('6815cb84117d356f34adb754')
     if (!findProduct) throw new CustomError("Invalid product slug", 400);
     return responce({
-      message: "successfully get product data",
+      message: "successfully get product data" + productslug,
       status: 200,
       data: JSON.parse(JSON.stringify(findProduct))
     });
